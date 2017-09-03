05722f6c836104e5a4aaf088acb132e6e5291079
england <englandpost@gmail.com>
2017-09-03 17:07:12 +0300

    missed order

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -2,6 +2,7 @@ class StatController < ApplicationController
   def medium
     @channels = Visitor.group(:medium).joins(:activity)
                   .select("medium, count(*) as visitors_count, sum(arpu) as arpu_sum")
+                  .order("visitors_count desc")
   end
 
   def activity
